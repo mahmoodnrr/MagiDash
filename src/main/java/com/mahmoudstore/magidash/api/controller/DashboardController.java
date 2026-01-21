18833597dfd8aab169d90d1519c0188093b6e7f2
Mahmoud <m.mahmoud2@edu.salford.ac.uk>
2026-01-21 00:24:10 +0000

    feat: added controller, model, and repository for dashboards

--- src/main/java/com/mahmoudstore/magidash/api/controller/DashboardController.java
+++ src/main/java/com/mahmoudstore/magidash/api/controller/DashboardController.java
@@ -1,4 +1,23 @@
 package com.mahmoudstore.magidash.api.controller;
 
+
+import com.mahmoudstore.magidash.api.model.Dashboard;
+import com.mahmoudstore.magidash.api.repository.DashboardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/dashboards")
 public class DashboardController {
+
+    @Autowired
+    DashboardRepository repository;
+
+    @GetMapping()
+    public List<Dashboard> getAll() {
+        return repository.findAll();
+    }
+
 }
--- src/main/java/com/mahmoudstore/magidash/api/model/Dashboard.java
+++ src/main/java/com/mahmoudstore/magidash/api/model/Dashboard.java
@@ -1,4 +1,55 @@
 package com.mahmoudstore.magidash.api.model;
 
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(name="dashboards")
 public class Dashboard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "createdAt")
+    private Date createdAt;
+
+    @Column(name = "updatedAt")
+    private Date updatedAt;
+
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
